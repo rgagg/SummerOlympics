8c837715eb4935addd2ebf07b6b28e8f8b824e8b
Richard Gagg <r.gagg@outlook.com>
2025-02-08 16:27:46 +1100

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Richard Gagg on 8/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
